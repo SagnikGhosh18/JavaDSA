75c3adaeb592fac0c3cdd53b2dd273467c7c22c3
Sagnik Ghosh <77972501+SagnikGhosh18@users.noreply.github.com>
2023-05-23 17:18:36 +0530

    Time: 2 ms (80.43%), Space: 44 MB (5.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,38 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int l = nums.length;
-        int f=1;
-        int low = -1*(int)(Math.pow(10,9));
-        int high = 1*(int)(Math.pow(10,9));
-        if(target<low || target>high || l<2 || l>(int)(Math.pow(10,4)))
-        {
-            f=0;
-        }
-        for(int i=0;i<l;i++)
-        {
-            if(nums[i]<low || nums[i]>high)
-            {
-                f=0;
-                break;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int rem = target - nums[i];
+            if(map.containsKey(rem)){
+                res[0]=i;
+                res[1]=map.get(rem);
+                return res;
             }
+            map.put(nums[i],i);
         }
-        if(f==1)
-        {
-            for(int i=0;i<l;i++)
-            {
-                for(int j=0;j<l;j++)
-                {
-                    if(i!=j && (nums[i]+nums[j]==target))
-                    {
-                        result[0]=i;
-                        result[1]=j;
-                    }
-                }
-            }
-        }
-        return result;
-        
+        return res;
         
     }
 }
