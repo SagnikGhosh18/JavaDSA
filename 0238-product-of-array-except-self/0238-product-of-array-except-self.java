5d9a4493eda84a19cb6bdb902cf83fb6c29c7693
Sagnik Ghosh <77972501+SagnikGhosh18@users.noreply.github.com>
2023-06-19 23:39:15 +0530

    Time: 2 ms (52.44%), Space: 53.6 MB (29.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int [] sm = new int[nums.length];
-    int [] pm = new int[nums.length];
-    int [] ans = new int[nums.length];
-    pm[0] = nums[0];
-    sm[nums.length-1] = nums[nums.length-1];
-
-    for(int i = 1; i<nums.length; i++){
-      pm[i] = pm[i-1]*nums[i];  
-    }
-
-    for(int i =nums.length-2; i>=0; i--)
-    {
-        sm[i] = sm[i+1]*nums[i];
-    }
-
-    for(int i =0; i<nums.length; i++)
-    {
-        if(i==0)
-         ans[i] = 1*sm[i+1];
-        else if(i==nums.length-1)
-        ans[i] = pm[i-1]*1;
-        else
-        ans[i] = sm[i+1]*pm[i-1];
-    }
-
-   return ans;
+        int[] pl = new int[nums.length];
+        int[] pr = new int[nums.length];
+        int[] res = new int[nums.length];
+        
+        pl[0] = 1;
+        pr[nums.length-1] = 1;
+        
+        for(int i=1;i<nums.length;i++){
+            pl[i] = nums[i-1] * pl[i-1];
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            pr[i] = nums[i+1] * pr[i+1];
+        }
+        for(int i=0;i<nums.length;i++)
+            res[i] = pl[i] * pr[i];
+        return res;
     }
 }
